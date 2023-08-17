ee2874a7074b1026883165a7da6d8806ed79eec3
Danil Ziborov <ziborov.daniel@yandex.ru>
2023-08-17 15:11:18 +0300

    make fixes after review #2

--- backend/utils/errors/errors.js
+++ backend/utils/errors/errors.js
@@ -1,7 +1,6 @@
 const BadRequestError = require('./bad-request-err');
 const ConflictError = require('./conflict-err');
 const ForbiddenError = require('./forbidden-err');
-const InternalServerError = require('./internal-server-err');
 const NotFoundError = require('./not-found-err');
 const UnauthorizedError = require('./unauthorized-err');
 
@@ -9,7 +8,6 @@ module.exports = {
   BadRequestError,
   ConflictError,
   ForbiddenError,
-  InternalServerError,
   NotFoundError,
   UnauthorizedError,
 };
